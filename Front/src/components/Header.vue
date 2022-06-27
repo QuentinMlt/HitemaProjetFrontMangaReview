f99c9f645ff12e6b384be90943d1d29334f50c4a
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-27 15:01:46 +0200

    ajout des differents fichier vue vide

---
+++ Front/src/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  
+  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <h3>MangaReview</h3>
+    <div class="collapse navbar-collapse ms-5" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'manga'}">Manga</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'anime'}">Anime</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'login'}">Sign in</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'register'}">Sign up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style>@import 'bootstrap/dist/css/bootstrap.min.css';</style>
