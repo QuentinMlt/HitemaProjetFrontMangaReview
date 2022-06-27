4e4f4b8e1e6430c77a6f70e8ce256e590198fc37
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-28 00:35:16 +0200

    ajout du service des mangas

---
+++ Front/src/services/mangaStore.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+
+function useMangaStore(){
+    return {getMangasList, getCoverByMangaId};
+}
+async function getMangasList() {
+    
+    const response = await axios.get("https://api.mangadex.org/manga").then(res => res).catch(err => err);
+    if (response.status !== 200) {
+        return null;
+    }
+
+
+    return response.data
+    
+    
+}
+
+async function getCoverByMangaId(mangaId) {
+    const reponse = await axios.get(`https://api.mangadex.org/cover/${{mangaId}}`).then(res => res).catch(err => err)
+    const coverFileName = response.attributes.fileName;
+    const coverLink = `https://api.mangadex.org/${{mangaId}}/${{coverFileName}}`
+    return coverLink;
+}
+
+
+export {useMangaStore};
