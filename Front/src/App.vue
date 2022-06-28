650c4b1ae8c92aab7c93b0fd528bfde93c295600
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-28 13:28:32 +0200

    Add Footer to App.vue

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
 import HeaderVue from './components/Header.vue';
+import Footer from './components/Footer.vue';
 import {useMangaStore} from "@/services/mangaStore";
-const {getMangasList} = useMangaStore();
-const {getCoverByMangaId} = useMangaStore();
+const {getMangasList, getCover} = useMangaStore();
 </script>
 
 <template>
   <HeaderVue></HeaderVue>
-  <div class="container mt-5">
+  <div class="container mt-5 mb-5">
     <router-view></router-view>
   </div>
+  <Footer></Footer>
 </template>
 
 <style>
