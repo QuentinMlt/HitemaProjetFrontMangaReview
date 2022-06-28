fefd621833a95cb4713223ac7110b3e1147ec7a9
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-28 15:06:42 +0200

    Update MangaById.vue

--- Front/src/components/MangaById.vue
+++ Front/src/components/MangaById.vue
@@ -17,21 +17,22 @@ onMounted( () => {
 
 <template>
 
-    <div class="container-sm rounded row p-5 section-block" >
-        <div class="col card text-white p-2 thumbnail">
-            <img src="https://mangadex.org/covers/a96676e5-8ae2-425e-b549-7f15dd34a6d8/3b736a05-2313-40b4-8af1-94b85b3efc72.jpg.256.jpg" class="card-img">
-        </div>
-        <div class="col-sm-8">
-            <h2>Title</h2>
-            <h3>By Author</h3>
-            <h4>Description :</h4><br>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <div class="row">
-            <p class="col-sm-6">Reviews : *stars* number of reviews</p>
-            <p class="col-sm-6">Comments : number of comments</p>
+    <div class="container-sm rounded p-5 section-block">
+        <div class="row">
+            <div class="col-sm-3 card text-white p-2 thumbnail">
+                <img src="https://mangadex.org/covers/a96676e5-8ae2-425e-b549-7f15dd34a6d8/3b736a05-2313-40b4-8af1-94b85b3efc72.jpg.256.jpg" class="card-img">
+            </div>
+            <div class="col-sm-8">
+                <h2>Title</h2>
+                <h3>By Author</h3>
+                <h4>Description :</h4><br>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div class="row">
+                <p class="col-sm-6">Reviews : *stars* number of reviews</p>
+                <p class="col-sm-6">Comments : number of comments</p>
+                </div>
             </div>
         </div>
-
     </div><br>
 
     <div class="container-sm rounded p-2 section-block" >
