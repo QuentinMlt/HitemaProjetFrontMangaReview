9b97cff4eee0aa9d6bc3883849b0aa6c116ebe69
slayerblast <hery_75@hotmail.fr>
2022-06-28 15:03:28 +0200

    dernier commit avant le merge vers la branche dev

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+import HeaderVue from './components/Header.vue';
 </script>
 
 <template>
+<HeaderVue></HeaderVue>
 <router-view></router-view>
 </template>
 
 <style>
 @import 'bootstrap/dist/css/bootstrap.min.css';
 </style>
+<link rel="stylesheet" href="style.css">
--- Front/src/components/Login.vue
+++ Front/src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
-<form>
+  <div class="box-container">
+  <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
@@ -11,7 +12,13 @@
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+  </form>
 </div>
-
+</div>
 </template>
+
+<style>
+.box-container{
+width: 500px;
+}
+</style>
