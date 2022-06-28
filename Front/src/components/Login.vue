a6ce800e30f087ad42fab179b0359ba727523fcb
slayerblast <hery_75@hotmail.fr>
2022-06-28 23:15:30 +0200

    amélioration graphique du formulaire de login

--- Front/src/components/Login.vue
+++ Front/src/components/Login.vue
@@ -44,26 +44,104 @@ export default {
 </script>
 
 <template>
-<div class="container">
-  <div class="box-container">
-  <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" v-model="email" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control"  v-model="password" placeholder="Password">
-  </div>
-  <button @click="submitForm()" class="btn btn-primary">Submit</button>
-  </form>
-</div>
+<div class="login-form">    
+    <form>
+		<div class="avatar"><i class="material-icons">&#xE7FF;</i></div>
+    	<h4 class="modal-title">Login to Your Account</h4>
+        <div class="form-group">
+            <input type="text" class="form-control" v-model="email" placeholder="Email" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" v-model="password" placeholder="Password" required="required">
+        </div>
+        <input @click="submitForm()" class="btn btn-primary btn-block btn-lg" value="Login">              
+    </form>			
+    <div class="text-center small">Don't have an account? <a><router-link :to="{name: 'register'}"><b>Sign up</b></router-link></a></div>
 </div>
 </template>
 
 <style>
-.box-container{
-width: 500px;
+body {
+	color: #999;
+	background: #f5f5f5;
+	font-family: 'Varela Round', sans-serif;
+}
+.form-control {
+	box-shadow: none;
+	border-color: #ddd;
+}
+.form-control:focus {
+	border-color: #4aba70; 
+}
+.login-form {
+	width: 350px;
+	margin: 0 auto;
+	padding: 30px 0;
+}
+.login-form form {
+	color: #434343;
+	border-radius: 1px;
+	margin-bottom: 15px;
+	background: #fff;
+	border: 1px solid #f3f3f3;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+	padding: 30px;
+}
+.login-form h4 {
+	text-align: center;
+	font-size: 22px;
+	margin-bottom: 20px;
+}
+.login-form .avatar {
+	color: #fff;
+	margin: 0 auto 30px;
+	text-align: center;
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	z-index: 9;
+	background: rgb(8,100,197);
+	padding: 15px;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+}
+.login-form .avatar i {
+	font-size: 62px;
+}
+.login-form .form-group {
+	margin-bottom: 20px;
+}
+.login-form .form-control, .login-form .btn {
+	min-height: 40px;
+	border-radius: 2px; 
+	transition: all 0.5s;
+}
+.login-form .close {
+	position: absolute;
+	top: 15px;
+	right: 15px;
+}
+.login-form .btn, .login-form .btn:active {
+	background: rgb(8,100,197) !important;
+	border: none;
+	line-height: normal;
+}
+.login-form .btn:hover, .login-form .btn:focus {
+	background: rgb(6,81,164) !important;
+}
+.login-form .checkbox-inline {
+	float: left;
+}
+.login-form input[type="checkbox"] {
+	position: relative;
+	top: 2px;
+}
+.login-form .forgot-link {
+	float: right;
+}
+.login-form .small {
+	font-size: 13px;
+}
+.login-form a {
+	color: rgb(8,100,197);
 }
 </style>
