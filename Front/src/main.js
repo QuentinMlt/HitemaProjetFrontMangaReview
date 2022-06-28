15a9d368e91aabab61b353d5a993af5d205e5293
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-28 21:15:16 +0200

    add and import fontawesome

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
 import App from './App.vue'
 import router from '@/router'
 
-createApp(App).use(router).mount('#app')
+library.add(faUserSecret)
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
