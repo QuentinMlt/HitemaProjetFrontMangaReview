974c15976dec9292562677142192363b3eaa6008
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-30 00:48:06 +0200

    change function named getMangasList to getMangaOrAnimeList

--- Front/src/services/mangaStore.js
+++ Front/src/services/mangaStore.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmJjYTNiN2U5NzJlNTg0NDM4MGQ0YzAiLCJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwidXNlcm5hbWUiOiJMYW1lZHVyIiwiaXNBZG1pbiI6dHJ1ZSwiY3JlYXRlZEF0IjoxNjU2NTI0ODMyMjAzLCJfX3YiOjAsImlhdCI6MTY1NjUzNTkzNiwiZXhwIjoxNjU2NjIyMzM2fQ.DlH3AXUX2XxMuz0ASIIhPZbR9aE_sJurV6YGTjwwXRY"
 
 function useMangaStore(){
-    return {getMangasList, getCover, addMangaOrAnime,getCategoriesList, getMangaById};
+    return {getMangaOrAnimeList, addMangaOrAnime,getCategoriesList, getMangaById};
 }
-async function getMangasList() {
+async function getMangaOrAnimeList() {
     
     const response = await axios.get("http://localhost:3001/manganime").then(res => res).catch(err => err);
     if (response.status !== 200) {
