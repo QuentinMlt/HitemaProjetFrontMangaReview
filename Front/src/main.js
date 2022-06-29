5bc12add08e9086ef3cda17ddd1151fb36c76d31
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-29 09:57:30 +0200

    delete fontawesome package and import in main.js

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { library } from '@fortawesome/fontawesome-svg-core'
 import App from './App.vue'
 import router from '@/router'
 
-library.add(faUserSecret)
-
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).mount('#app')
