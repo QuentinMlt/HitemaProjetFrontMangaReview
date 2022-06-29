28fa602fbc51fbe679086fd39aadff1d3703f137
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-29 23:31:40 +0200

    create function getCategoriesList()

--- Front/src/services/mangaStore.js
+++ Front/src/services/mangaStore.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-
+let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmJjYTNiN2U5NzJlNTg0NDM4MGQ0YzAiLCJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwidXNlcm5hbWUiOiJMYW1lZHVyIiwiaXNBZG1pbiI6dHJ1ZSwiY3JlYXRlZEF0IjoxNjU2NTI0ODMyMjAzLCJfX3YiOjAsImlhdCI6MTY1NjUzNTkzNiwiZXhwIjoxNjU2NjIyMzM2fQ.DlH3AXUX2XxMuz0ASIIhPZbR9aE_sJurV6YGTjwwXRY"
 
 function useMangaStore(){
-    return {getMangasList, getCover};
+    return {getMangasList, getCover, addMangaOrAnime,getCategoriesList};
 }
 async function getMangasList() {
     
@@ -26,6 +26,15 @@ async function getCover(mangaId,coverId) {
     }).catch(err => err);
     return coverLink;
     
+}
+
+
+async function getCategoriesList() {
+    const categoriesList = await axios.get(`http://localhost:3001/genres`).then(res => res).catch(err => err);
+    if (categoriesList.status !== 200) {
+        return null;
+    }
+    return categoriesList.data;
 }
 
 
