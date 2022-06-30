2833c750e40410c671b9a199844da1869e9cc879
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-30 21:53:33 +0200

    delete function about reviews

--- Front/src/services/mangaStore.js
+++ Front/src/services/mangaStore.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmJjYTNiN2U5NzJlNTg0NDM4MGQ0YzAiLCJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwidXNlcm5hbWUiOiJMYW1lZHVyIiwiaXNBZG1pbiI6dHJ1ZSwiY3JlYXRlZEF0IjoxNjU2NTI0ODMyMjAzLCJfX3YiOjAsImlhdCI6MTY1NjUzNTkzNiwiZXhwIjoxNjU2NjIyMzM2fQ.DlH3AXUX2XxMuz0ASIIhPZbR9aE_sJurV6YGTjwwXRY"
 
 function useMangaStore(){
-    return {getMangaOrAnimeList, addMangaOrAnime,getCategoriesList, getMangaOrAnimeById,getMangaOrAnimeByUser, deleteMangaOrAnime};
+    return {getMangaOrAnimeList, addMangaOrAnime, getMangaOrAnimeById,getMangaOrAnimeByUser, deleteMangaOrAnime};
 }
 async function getMangaOrAnimeList() {
     
@@ -55,15 +55,6 @@ async function addMangaOrAnime(entity)
     }
     console.log("post réussi")
 }
-
-async function getCategoriesList() {
-    const categoriesList = await axios.get(`http://localhost:3001/genres`).then(res => res).catch(err => err);
-    if (categoriesList.status !== 200) {
-        return null;
-    }
-    return categoriesList.data;
-}
-
 
 
 
