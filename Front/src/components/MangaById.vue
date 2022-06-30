8f3497c5354a14f1800c3e0839199335d2b84824
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-30 10:45:01 +0200

    add putReview() function

--- Front/src/components/MangaById.vue
+++ Front/src/components/MangaById.vue
@@ -1,20 +1,55 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import Joi from 'joi';
 import {useRoute} from "vue-router";
 import StarRating from 'vue-star-rating'
 import {useMangaStore} from "@/services/mangaStore";
 
-const {getMangaById} = useMangaStore();
+const {getMangaById, putReview} = useMangaStore();
 
 const manga = ref("");
+const mangaId = ref("");
+const loading = ref(true);
+const rating = ref("");
+const newComment = ref("");
+const errorSaisie = ref('');
+
 
 onMounted( async () => {
     const route = useRoute();
-    const mangaId = ref(route.params.id);
+    mangaId.value = route.params.id;
     console.log(mangaId.value);
     manga.value = await getMangaById(mangaId.value);
     console.log(manga.value)
+    loading.value = false
 })
+
+async function setReview() {
+   const scheme = Joi.object({
+            score: Joi.number().integer().min(0).max(5).required(),
+            manganimeId: Joi.string().required()
+        });
+        
+        const payload = {
+            score: rating.value,
+            manganimeId: manga.value._id
+        };
+
+        const {value, error} = scheme.validate(payload);
+        if (error) {
+            console.log("PAS REUSSI")
+            errorSaisie.value = error.message;
+            return;
+        }
+        else{
+            console.log("Reussi");
+            await putReview(payload)
+        }
+    
+}
+
+
+
 
 
 
@@ -22,19 +57,20 @@ onMounted( async () => {
 
 <template>
 
-    <div class="container-sm rounded p-5 section-block">
+    <div class="container-sm rounded p-5 section-block" v-if="!loading">
         <div class="row">
             <div class="col-sm-3 card text-white p-2 thumbnail"><!-- MANGA IMG -->
                 <img :src="manga.imageUrl" class="card-img">
             </div>
             <div class="col-sm-8"><!-- MANGA BIO -->
                 <h2>{{manga.name}}</h2>
-                <h3>{{manga.authorId}}</h3>
+                <h3>by {{manga.authorId.username}}</h3>
                 <h4>Description :</h4><br>
                 <p>{{manga.description}}</p>
                 <div class="row">
-                <p class="col-sm-6">Reviews : *stars* number of reviews</p>
-                <p class="col-sm-6">Comments : number of comments</p>
+                    <p class="col-sm-6">Reviews : *stars* number of reviews</p>
+                    <p class="col-sm-6">Comments : number of comments</p>
+                    <star-rating @click="setReview()" v-model:rating="rating" v-bind:star-size="30"></star-rating>
                 </div>
             </div>
         </div>
@@ -44,15 +80,12 @@ onMounted( async () => {
         <h3>Reviews Section</h3>
         <div class="ms-3 p-3 section-comment">
             <strong>Your opinion :</strong>
-            <form><!-- ADD COMMENTS & REVIEWS -->
+            <!-- ADD COMMENTS & REVIEWS -->
                 <div class="form-group mb-2">
                     <label for="Comments">Comments :</label>
-                    <textarea type="text" class="form-control" id="Comments"></textarea>
+                    <textarea type="text" class="form-control" id="Comments" v-model="newComment"></textarea>
                 </div>
-                <star-rating v-model:rating="rating" v-bind:star-size="30"></star-rating>
-            
-            </form>
-            
+                <button class="btn btn-success" @click="AddComment(newComment)">Add your comment</button>
             <hr>
         </div>
         
--- Front/src/services/mangaStore.js
+++ Front/src/services/mangaStore.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmJjYTNiN2U5NzJlNTg0NDM4MGQ0YzAiLCJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwidXNlcm5hbWUiOiJMYW1lZHVyIiwiaXNBZG1pbiI6dHJ1ZSwiY3JlYXRlZEF0IjoxNjU2NTI0ODMyMjAzLCJfX3YiOjAsImlhdCI6MTY1NjUzNTkzNiwiZXhwIjoxNjU2NjIyMzM2fQ.DlH3AXUX2XxMuz0ASIIhPZbR9aE_sJurV6YGTjwwXRY"
 
 function useMangaStore(){
-    return {getMangaOrAnimeList, addMangaOrAnime,getCategoriesList, getMangaById};
+    return {getMangaOrAnimeList, addMangaOrAnime,getCategoriesList, getMangaById, putReview};
 }
 async function getMangaOrAnimeList() {
     
@@ -47,6 +47,13 @@ async function getCategoriesList() {
         return null;
     }
     return categoriesList.data;
+}
+
+async function putReview(review) {
+    const response = await axios.put("http://localhost:3001/reviews", {"score": review.score, "manganimeId": review.manganimeId}, {headers: {Authorization: 'Bearer ' + token}}).then(res => res).catch(err => err);
+    if (response.status !== 201) {
+        return null;
+    }
 }
 
 
