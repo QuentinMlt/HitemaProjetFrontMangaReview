786ba354764af2a4ae8a593f1e21d3c86b2d2eb1
slayerblast <hery_75@hotmail.fr>
2022-06-30 16:23:53 +0200

    premier version du dashboard

---
+++ Front/src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="rounded p-5" id="container">
+        <h2 id="title">MODERATOR PANEL</h2><br>
+        <div class="row" id="listContainer"> <!-- Display -->
+                <div class="" id="box-user">
+
+                </div>
+        </div>
+    </div>
+</template>
+<style>
+#container {
+    background: white;
+    box-shadow: 1px 1px 20px 1px rgba(0, 0, 0, .5);
+}
+#title {
+    color: orange;
+    text-align: center;
+}
+#box-user{
+    padding: 300px;
+    border: 2px solid black;
+}
+</style>
